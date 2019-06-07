2a7cd8071da0f9ad2df0cc9cb559b10c5bfc06b5
Joydip Sarkar <joydip.sarkar01@gmail.com>
2019-06-07 18:58:24 +0530

    added

---
+++ HTML/html-v2/assets/js/script.js
@@ -0,0 +1,64 @@
+(function ($) {
+    "use strict";
+    
+    /* Single banner slider scrpt start */
+    var singleAnimatedBanner = $('.singleAnimatedBanner');
+    singleAnimatedBanner.owlCarousel({
+        animateOut: 'fadeOut',
+        animateIn: 'flipInX',
+        items: 1,
+        margin: 0,
+        loop: true,
+        stagePadding: 0,
+        smartSpeed: 450,
+        mouseDrag: true,
+        touchDrag: true,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left fa-2x fa-fw" aria-hidden="true"></i>', '<i class="fa fa-angle-right fa-2x fa-fw" aria-hidden="true"></i>'], //we will be
+    });
+
+    singleAnimatedBanner.on('changed.owl.carousel', function (event) {
+        //console.log("test");
+        /*var item = event.item.index - 2;
+        // first removing animation for all captions
+        $('h2').removeClass('animated fadeInUp');
+        $('.owl-item').not('.cloned').eq(item).find('h2').addClass('animated fadeInUp');*/
+
+    });
+    /* Single banner slider scrpt ended */
+
+
+    /* home team slider scrpt start */
+    var homeTeamMultiSlider = $('.homeTeamMultiSlider');
+    homeTeamMultiSlider.owlCarousel({
+        //animateOut: 'fadeOut',
+        //animateIn: 'flipInX',
+        items: 1,
+        margin:30,
+        loop: false,
+        stagePadding:0,
+        smartSpeed: 450,
+        nav: false,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left fa-2x fa-fw" aria-hidden="true"></i>', '<i class="fa fa-angle-right fa-2x fa-fw" aria-hidden="true"></i>'],
+        responsiveClass:true,
+        responsive:{
+            0:{ items:1, }, 600:{ items:2, nav: true }, 1000:{  items:4 }
+        }
+    });
+    /* home team slider scrpt ended */
+
+    
+    /* footer expand / collaps script start */
+    $(".footExpandBtn").click(function(){
+        $(this).toggleClass("expand");
+        $(".expandedSection").toggleClass("expand");
+    });
+    /* footer expand / collaps script ended */
+
+
+
+
+
+})(window.jQuery);
+
