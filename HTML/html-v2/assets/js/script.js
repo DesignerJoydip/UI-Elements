a94aa4220125cb3bc6b80093c07bd08b847fa8e4
Joydip Sarkar <joydip.sarkar01@gmail.com>
2019-07-17 19:08:09 +0530

    added

--- HTML/html-v2/assets/js/script.js
+++ HTML/html-v2/assets/js/script.js
@@ -3,43 +3,6 @@
 
     new WOW().init();
     
-    /* Single banner slider scrpt start */
-    var singleAnimatedBanner = $('.singleAnimatedBanner');
-    singleAnimatedBanner.owlCarousel({
-        //animateOut: 'fadeOut',
-        //animateIn: 'flipInX',
-        items: 1,
-        margin: 0,
-        loop: true,
-        stagePadding: 0,
-        smartSpeed: 450,
-        mouseDrag: true,
-        touchDrag: true,
-        nav: true,
-        navText: ['<i class="fa fa-angle-left fa-2x fa-fw" aria-hidden="true"></i>', '<i class="fa fa-angle-right fa-2x fa-fw" aria-hidden="true"></i>'], //we will be
-    });
-
-    singleAnimatedBanner.on('changed.owl.carousel', function (event) {
-        //console.log("test");
-        /*var item = event.item.index - 2;
-        // first removing animation for all captions
-        $('h2').removeClass('animated fadeInUp');
-        $('.owl-item').not('.cloned').eq(item).find('h2').addClass('animated fadeInUp');*/
-
-    });
-    /* Single banner slider scrpt ended */
-
-    
-    /* footer expand / collaps script start */
-    $(".footExpandBtn").click(function(){
-        $(this).toggleClass("expand");
-        $(".expandedSection").toggleClass("expand");
-    });
-    /* footer expand / collaps script ended */
-
-
-    
-
 
 })(window.jQuery);
 
