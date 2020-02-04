b748d563791e0e9360ef82f3206c205c10a2e381
Joydip Sarkar <joydip.sarkar01@gmail.com>
2020-02-04 18:38:25 +0530

    html added

---
+++ HTML/html-ui/assets/js/custom-script.js
@@ -0,0 +1,26 @@
+/* window load script start */
+jQuery(window).on("load", function (e) { 
+
+    /* preloader script */
+    jQuery("#preloader").fadeOut(); 
+
+});
+/* window load script ended */
+
+
+
+$(".code-previewer-area").each(function(){
+	$codeTabID = $(this).find(".btn-group-toggle .btn.active").children().attr("id");
+	$codeTabContent = $(this).find(".code-previewer-content.active").attr("data-tabContentName");
+
+	$(this).find(".btn-group-toggle .btn").each(function(){
+		$(this).click(function(){
+			$(this).find(".btn-group-toggle .btn").removeClass("active");
+			$(this).addClass("active");
+			$getActiveTabID = $(this).children().attr("id");
+			console.log($getActiveTabID);
+			$(this).parent().parent().find(".code-previewer-content").removeClass("active");
+			$(this).parent().parent().find("[data-tabContentName="+$getActiveTabID+"]").addClass("active");
+		});
+	});
+});
