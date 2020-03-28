f3823513a6fa79c2610016dfa43a738e4d0b075e
Joydip <joydip.sarkar01@gmail.com>
2020-03-28 09:43:47 +0530

    added

--- office/KAEC_BIK/HTML/kaec-bik/assets/js/script.js
+++ office/KAEC_BIK/HTML/kaec-bik/assets/js/script.js
@@ -150,7 +150,7 @@
     // home-city-benefit-slider script start-------------
     if ($(".home-city-benefit-slider").length) {
         $('.home-city-benefit-slider').slick({
-            slidesToShow: 4,
+            slidesToShow: 5,
             slidesToScroll: 1,
             dots: false,
             centerMode: false,
@@ -159,7 +159,7 @@
                 {
                     breakpoint: 1200,
                     settings: {
-                        slidesToShow: 4,
+                        slidesToShow: 5,
                     }
                 },
                 {
@@ -196,7 +196,9 @@
 
     // custom scrollbar script start ---------------------------
     $(".custom-scrollbar").mCustomScrollbar({
-        theme: "minimal"
+        //theme: "minimal",
+        autoHideScrollbar:false,
+		theme:"rounded"
     });
     // custom scrollbar script ended ---------------------------
 
@@ -315,3 +317,77 @@ $('.select').each(function () {
     });
 
 });
+
+
+
+
+// community start
+$(document).ready(function () {
+    $('.details-toggle').click(function () {
+        var collapse_content_selector = $(this).attr('href');
+        var toggle_switch = $(this);
+        $(collapse_content_selector).toggle(function () {
+            if ($(this).css('display') == 'none') {
+                toggle_switch.html('View More');
+            } else {
+                toggle_switch.html('View Less');
+            }
+        });
+    });
+
+});
+
+function classToggle() {
+    this.classList.toggle('active');
+}
+document.querySelector('.details-toggle').addEventListener('click', classToggle);
+// community end
+
+// Property Search JS Start
+
+function wcqib_refresh_quantity_increments() {
+    jQuery("div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)").each(function (a, b) {
+        var c = jQuery(b);
+        c.addClass("buttons_added"), c.children().first().before('<input type="button" value="-" class="minus" />'), c.children().last().after('<input type="button" value="+" class="plus" />')
+    })
+}
+String.prototype.getDecimals || (String.prototype.getDecimals = function () {
+    var a = this,
+        b = ("" + a).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+    return b ? Math.max(0, (b[1] ? b[1].length : 0) - (b[2] ? +b[2] : 0)) : 0
+}), jQuery(document).ready(function () {
+    wcqib_refresh_quantity_increments()
+}), jQuery(document).on("updated_wc_div", function () {
+    wcqib_refresh_quantity_increments()
+}), jQuery(document).on("click", ".plus, .minus", function () {
+    var a = jQuery(this).closest(".quantity").find(".qty"),
+        b = parseFloat(a.val()),
+        c = parseFloat(a.attr("max")),
+        d = parseFloat(a.attr("min")),
+        e = a.attr("step");
+    b && "" !== b && "NaN" !== b || (b = 0), "" !== c && "NaN" !== c || (c = ""), "" !== d && "NaN" !== d || (d = 0), "any" !== e && "" !== e && void 0 !== e && "NaN" !== parseFloat(e) || (e = 1), jQuery(this).is(".plus") ? c && b >= c ? a.val(c) : a.val((b + parseFloat(e)).toFixed(e.getDecimals())) : d && b <= d ? a.val(d) : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())), a.trigger("change")
+});
+
+
+
+
+
+function classToggle() {
+    this.classList.toggle('active');
+}
+document.querySelector('.all-property--button').addEventListener('click', classToggle);
+
+$(function () {
+    $("#slider-range").slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [75, 300],
+        slide: function (event, ui) {
+            $("#amount").val("" + ui.values[0] + " - " + ui.values[1]);
+        }
+    });
+    $("#amount").val("" + $("#slider-range").slider("values", 0) +
+        " - " + $("#slider-range").slider("values", 1));
+});
+// Property Search JS End
